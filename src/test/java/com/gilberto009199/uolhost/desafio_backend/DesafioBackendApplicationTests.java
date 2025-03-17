b81eec7013fa234b538edb1aea1c694064d9eeb9
gilberto-009199 <gilberto.tec@vivaldi.net>
2025-03-17 02:11:26 -0300

    init: project structure

---
+++ src/test/java/com/gilberto009199/uolhost/desafio_backend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilberto009199.uolhost.desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
